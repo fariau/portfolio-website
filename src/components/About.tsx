7c075902bbf6e017a319d66a3be13808cc8f3a53
fariau <usmanfaria88@gmail.com>
2024-12-31 15:12:27 +0500

    done

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <div id='about' className='container pt-32'>
       <h2 className='text-4xl md:text-5xl'>About me</h2>
-      <p className='text-gray-500 pt-4'>I'm a student of GIAIC, pursing a course in Artificial 
+      <p className='text-gray-500 pt-4'>I&apos;m a student of GIAIC, pursing a course in Artificial 
         Intaligence, web 3.0 Metaverse. pessionate about tecnology and constantly learning new skills 
         to stay up-to-date with the latest innovation.</p>
     </div>
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -11,7 +11,7 @@ const Hero = () => {
         <div className='hidden lg:block'></div>
         <div className='text-[80px] sm:text-[100px] font-bold leading-tight flex justify-center items-center'>
           <div>
-            <p data-aos="zoom-out-left">I'm</p>
+            <p data-aos="zoom-out-left">I&apos;m</p>
             <p data-aos="zoom-out-left">Faria</p>
             <p data-aos="zoom-out-left">Usman</p>
           </div>
